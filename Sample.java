77297ca2d1039d0b365d7c069d233f865de36ef6
Aravind <aravindpandiyarajan2004@gmail.com>
2024-03-12 10:25:18 +0530

    Sample modified by b1

--- Sample.java
+++ Sample.java
@@ -1,6 +1,8 @@
+
 class Sample{
   public static void main(String args[]){
     System.out.println("Hello, Aravind!");
+   System.out.println("Have a nice day");
     System.out.println("Welcome to relevantz");
   }
 }
